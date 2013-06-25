cc3611216c706d5c4492619ce70a67e2ed2c0305
ajlopez <webmaster@ajlopez.com>
2013-06-25 19:14:55 -0300

    SimpleTask first test

---
+++ Src/TRun.Core.Tests/SimpleTaskTests.cs
@@ -0,0 +1,20 @@
+﻿namespace TRun.Core.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SimpleTaskTests
+    {
+        [TestMethod]
+        public void RunSimpleTask()
+        {
+            SimpleTask task = new SimpleTask();
+
+            Assert.IsTrue(task.Run());
+        }
+    }
+}
---
+++ Src/TRun.Core/SimpleTask.cs
@@ -0,0 +1,15 @@
+﻿namespace TRun.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class SimpleTask
+    {
+        public bool Run()
+        {
+            return true;
+        }
+    }
+}
