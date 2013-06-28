43a79d977d079bcc4a34c2376e45f4e19f8ffe66
ajlopez <webmaster@ajlopez.com>
2013-06-28 10:34:36 -0300

    TargetReader first test

---
+++ Src/TRun.Core.Tests/TargetReaderTests.cs
@@ -0,0 +1,24 @@
+﻿namespace TRun.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TargetReaderTests
+    {
+        [TestMethod]
+        public void ReadSimpleTask()
+        {
+            TargetReader reader = new TargetReader();
+
+            var result = reader.Read("target");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("target", result[0].Name);
+        }
+    }
+}
---
+++ Src/TRun.Core/TargetReader.cs
@@ -0,0 +1,15 @@
+﻿namespace TRun.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TargetReader
+    {
+        public IList<Target> Read(string text)
+        {
+            return new List<Target>() { new Target(text, new ITask[] { }) };
+        }
+    }
+}
